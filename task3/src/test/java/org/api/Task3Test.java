6f45fdd71bda1a33423908a8ba028e2b8a11cafb
chuhalova222 <chuhalovaveronika@gmail.com>
2024-10-26 11:05:06 +0300

    Changes

--- task3/src/test/java/org/api/Task3Test.java
+++ task3/src/test/java/org/api/Task3Test.java
@@ -31,7 +31,6 @@ public class Task3Test extends BaseTest {
 
         var responseAsString = response.asString();
         log.info("Response:\n" + prettyPrint(responseAsString));
-        //todo need to fix this verification, do not work now...
 //      3. Validate JSON schema for this API call
         JsonUtil.validateJsonAgainstSchema(responseAsString, "/schema.json");
     }
